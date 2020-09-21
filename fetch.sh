aaa89cf9421c07f19427546fef475d2bdb9e1331
Kazeem Hakeem <hakymulla@gmail.com>
2020-09-21 14:16:30 +0100

    Script to fecth data

---
+++ fetch.sh
@@ -0,0 +1,12 @@
+echo 'Downloading data'
+mkdir data/;
+curl https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/ftmp4cvtmb-1.zip -o data/alldata;
+unzip data/alldata -d data/;
+
+mkdir data/images
+
+mv data/'First Set'/'100x Normal Oral Cavity Histopathological Images'/* data/images
+mv data/'First Set'/'100x OSCC Histopathological Images'/* data/images
+mv data/'Second Set'/'400x Normal Oral Cavity Histopathological Images'/* data/images
+mv data/'Second Set'/'400x OSCC Histopathological Images'/* data/images
+rm -rf data/'First Set'/ data/'First Set'/
